8a27bb2ede9ca42e1d2ebd5a16b7f357ec075d5c
Philippe Traverse <philippe.traverse@gmail.com>
2016-06-02 23:58:55 -0700

    stripped down

--- js/helloworld.jsx
+++ js/helloworld.jsx
@@ -1,22 +1,55 @@
 'use strict';
 
-var $ = require('jquery');
+var classNames = require('classnames');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-$(document).on("ready", function() {
-    ReactDOM.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('example')
-    );
+ReactDOM.render(
+    <h4>ShellWidget Example</h4>,
+    document.getElementById('firstTest')
+);
 
-    ReactDOM.render(
-        <button class="btn btn-primary" id="doIt">Do Something</button>,
-        document.getElementById('firstTest')
-    );
-
-    ReactDOM.render(
-        <pre id="debug"></pre>,
-        document.getElementById('debug_parent')
-    );
+var ShellWidget = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ShellWidgetButton text={this.props.text} action={this.props.action} />
+                <ShellWidgetOutput />
+            </div>
+        );
+    }
 });
+
+var ShellWidgetButton = React.createClass({
+    render: function() {
+        var btnClass = classNames({
+            'btn': true,
+            'btn-primary': true,
+            'btn-shell-widget': true
+        });
+        return (
+            <button action={this.props.action} className={btnClass}>
+                {this.props.text}
+            </button>
+        );
+    }
+});
+
+var ShellWidgetOutput = React.createClass({
+    render: function() {
+        return (
+            <pre className="debug">
+            </pre>
+        );
+    }
+});
+
+ReactDOM.render(
+    <ShellWidget text="Click Me" action="echo im in ur shell!" />,
+    document.getElementById('secondTest')
+);
+
+ReactDOM.render(
+    <ShellWidget text="List Files" action="ls -la" />,
+    document.getElementById('thirdTest')
+);
--- renderer.js
+++ renderer.js
@@ -2,23 +2,35 @@
 
 var $ = require('./bower_components/jquery/dist/jquery.js');
 var spawn = require('child_process').spawn;
+var _ = require('./bower_components/underscore/underscore.js');
 
 $(document).on("ready", function() {
-
-    var spawnDebug = function(obj) {
-        $('#debug').append(String(obj));
+    /**
+     * Adds String of obj to closest <pre class="debug"> to button
+     *
+     * @param obj
+     * @param button
+     */
+    var selfDebug = function(obj, button) {
+        var pre = $(button).siblings('pre.debug');
+        $(pre).append(String(obj));
     };
 
-    $('button#doIt').on('click', function () {
-        var child = spawn('echo', [
-            'asdfasdfasdf'
-        ]);
+    /**
+     * Bind btn-shell-widget Buttons
+     * Takes action of button and runs it as shell script, piping stdout and stderr to output div
+     */
+    $('button.btn-shell-widget').on('click', function(e) {
+        e.preventDefault();
+        var button = $(this);
+        var action = $(this).attr('action');
+        var actionCommand = action.split(/[ ]+/);
+        var child = spawn(actionCommand[0], _.last(actionCommand, actionCommand.length - 1));
         child.stdout.on('data', function (chunk) {
-            spawnDebug(chunk);
+            selfDebug(chunk, button);
         });
-
         child.stderr.on('data', function (err) {
-            spawnDebug(err);
+            selfDebug(err, button);
         });
     });
 
